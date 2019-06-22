2712893aa211dfe5d5228dcf450ccf8bcc42805f
Christa <christahyde@yahoo.com>
2019-06-22 11:32:10 -0600

    Updates

---
+++ precache-manifest.d603e1fb64dce4467b5708eee50616b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c1427ea4b03b79bdcab26873f42336",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "3087704934ebde2c2d45",
+    "url": "/ClickyGame/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3087704934ebde2c2d45",
+    "url": "/ClickyGame/static/js/main.88c297ef.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
